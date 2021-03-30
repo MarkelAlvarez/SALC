3e30c139d7563ab638ccfd450cb196a8040a3b26
MarkelAlvarez <markelalvarez99@gmail.com>
2021-03-30 10:19:07 +0200

    Spring Initializr

---
+++ src/main/java/com/ahsoka/SALC/SalcApplication.java
@@ -0,0 +1,13 @@
+package com.ahsoka.SALC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalcApplication.class, args);
+	}
+
+}
