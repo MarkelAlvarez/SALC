b39d1608115cdc7306a0efddc70223472145d71f
jaroave <80476897+jaroave@users.noreply.github.com>
2021-04-30 11:59:38 +0200

    Update UserRepository.java

--- src/main/java/com/ahsoka/SALC/user_model/persistance/repository/UserRepository.java
+++ src/main/java/com/ahsoka/SALC/user_model/persistance/repository/UserRepository.java
@@ -1,1 +1,7 @@
+package com.ahsoka.SALC.user_model.persistance.repository;
 
+@Repository
+public interface UserRepository {
+	public User findByEmail(String email);
+	public List<User> findAll();
+}
