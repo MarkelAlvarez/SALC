493a95437e54e5a9e8c67bf171ed839078f5ad96
MarkelAlvarez <markelalvarez99@gmail.com>
2021-05-03 19:07:57 +0200

    EmailValidator implemented

--- src/main/java/com/ahsoka/SALC/user_model/controller/EmailValidator.java
+++ src/main/java/com/ahsoka/SALC/user_model/controller/EmailValidator.java
@@ -1,5 +1,29 @@
 package com.ahsoka.SALC.user_model.controller;
 
-public class EmailValidator {
-	
+import lombok.NoArgsConstructor;
+
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@NoArgsConstructor
+public class EmailValidator implements Predicate<String> {
+
+    Matcher matcher;
+    private static final String EMAIL_REGEX = "^(.+)@salc.org";
+    Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
+    @Override
+    public boolean test(String email) {
+
+        matcher = pattern.matcher(email);
+        if (matcher.matches())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
