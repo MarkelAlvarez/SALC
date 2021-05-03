ef0234c0aa61f613acc8d8b9d66639ca26bdf83a
MarkelAlvarez <markelalvarez99@gmail.com>
2021-05-03 19:13:15 +0200

    Added PagingAndSortingRepository dependancy

---
+++ src/test/java/com/ahsoka/SALC/user_model/persistance/repository/UserRepositoryIT.java
@@ -0,0 +1,35 @@
+package com.ahsoka.SALC.user_model.persistance.repository;
+
+import com.ahsoka.SALC.user_model.persistance.entity.User;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class UserRepositoryIT {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Disabled
+    @Test
+    void testFindByEmail() {
+        assertTrue(userRepository.findByEmail("test_admin@salc.org").isPresent());
+        System.out.println(userRepository.findByEmail("test_admin@salc.org"));
+    }
+
+    @Disabled
+    @Test
+    void testFindAll() {
+        assertFalse(userRepository.findAll().isEmpty());
+        List<User> userList = userRepository.findAll();
+        for(User user : userList)
+            System.out.println(user);
+    }
+}
