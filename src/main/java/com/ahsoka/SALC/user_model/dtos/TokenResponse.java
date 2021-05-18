135fdbb5e560d5ad01d0b101ce8202a709893500
jaroave <80476897+jaroave@users.noreply.github.com>
2021-05-18 19:49:36 +0200

    Update TokenResponse.java

--- src/main/java/com/ahsoka/SALC/user_model/dtos/TokenResponse.java
+++ src/main/java/com/ahsoka/SALC/user_model/dtos/TokenResponse.java
@@ -1,5 +1,20 @@
 package com.ahsoka.SALC.user_model.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenResponse {
-	
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}	
 }
