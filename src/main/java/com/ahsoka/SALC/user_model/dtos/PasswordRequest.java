7ffd9f44a42a71ada50d7cf66a0fd328abf163b1
MarkelAlvarez <markelalvarez99@gmail.com>
2021-05-18 19:01:36 +0200

    PasswordRequest and its test

--- src/main/java/com/ahsoka/SALC/user_model/dtos/PasswordRequest.java
+++ src/main/java/com/ahsoka/SALC/user_model/dtos/PasswordRequest.java
@@ -1,12 +1,12 @@
 package com.ahsoka.SALC.user_model.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotBlank;
 
-
 @Data
-
+@AllArgsConstructor
 public class PasswordRequest {
 
     @NotNull
@@ -16,5 +16,4 @@ public class PasswordRequest {
     @NotNull
     @NotBlank
     private String password;
-	
 }
