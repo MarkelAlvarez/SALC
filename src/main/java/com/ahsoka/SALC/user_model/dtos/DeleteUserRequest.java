132a97ae017e411bd85f1c9b3ab03980c842a951
MarkelAlvarez <markelalvarez99@gmail.com>
2021-05-18 19:24:55 +0200

    DeleteUserRequest and its test

--- src/main/java/com/ahsoka/SALC/user_model/dtos/DeleteUserRequest.java
+++ src/main/java/com/ahsoka/SALC/user_model/dtos/DeleteUserRequest.java
@@ -1,14 +1,13 @@
 package com.ahsoka.SALC.user_model.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import javax.validation.constraints.NotNull;
 
-
 @Data
-
+@AllArgsConstructor
 public class DeleteUserRequest {
 
     @NotNull
     private String email;
-
 }
