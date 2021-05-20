193f6df976f2fe580c1ce352eeda5bdaf9c72836
Luis Egui <misionpage@gmail.com>
2021-05-20 18:37:31 +0200

    * Agregado nuevos métodos "readUserByEmail(...)", "updateUserEmail(...)", "updateUserPassword(...)"

--- src/main/java/com/ahsoka/SALC/user_model/controller/UserController.java
+++ src/main/java/com/ahsoka/SALC/user_model/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ahsoka.SALC.user_model.controller;
 
 import com.ahsoka.SALC.user_model.dtos.NewUserRequest;
 import com.ahsoka.SALC.user_model.dtos.TokenResponse;
+import com.ahsoka.SALC.user_model.dtos.UpdateUserRequest;
 import com.ahsoka.SALC.user_model.filter.JwtService;
 import com.ahsoka.SALC.user_model.persistance.entity.User;
 import com.ahsoka.SALC.user_model.service.Response;
@@ -10,10 +11,9 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -40,6 +40,7 @@ public class UserController {
     @PostMapping(value = AUTHENTICATION)
     public Optional<TokenResponse> login(@RequestBody User user) {
         Optional<String> token = userService.login(user.getEmail(), user.getPassword());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         if(token.isPresent())
             return Optional.of(new TokenResponse(token.get()));
@@ -48,20 +49,52 @@ public class UserController {
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PostMapping(value = USERS + "add/")
+    @PostMapping(value = USERS)
     public String createUser(@Valid @RequestBody NewUserRequest user) {
         Response response = userService.createUser(user.toUser());
 
         if(response.equals(Response.OK))
             return String.valueOf(HttpServletResponse.SC_CREATED);
-        else {
+        else
             return HttpServletResponse.SC_EXPECTATION_FAILED + " " + response;
-        }
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping(value = USERS)
     public Stream<User> readAll() {
         return userService.readAll();
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping(value = USERS + "email/")
+    public Optional<User> readUserByEmail(@RequestParam String email) {
+        return userService.readUserByEmail(email);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PatchMapping(value = USERS + "email/")
+    public String updateUserEmail(@RequestBody UpdateUserRequest userRequest, @RequestParam String referenceEmail) {
+        Response response = userService.updateUserEmail(userRequest.toUser(), referenceEmail);
+
+        if(response.equals(Response.OK))
+            return String.valueOf(HttpServletResponse.SC_OK);
+        else if(response.equals(Response.REFERENCE_EMAIL_DOESNT_EXIST))
+            return HttpServletResponse.SC_NO_CONTENT + " " + response;
+        else
+            return HttpServletResponse.SC_CONFLICT + " " + response;
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_WORKER', 'ROLE_CATALOGER', 'AUTHENTICATED')")
+    @PatchMapping(value = USERS + "password/")
+    public String updateUserPassword(@RequestBody UpdateUserRequest userRequest, @RequestParam String referenceEmail) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Response response = userService.updateUserPassword(userRequest.toUser(), referenceEmail, auth.getName());
+
+        if(response.equals(Response.OK))
+            return String.valueOf(HttpServletResponse.SC_OK);
+        else if(response.equals(Response.REFERENCE_EMAIL_DOESNT_EXIST))
+            return HttpServletResponse.SC_NO_CONTENT + " " + response;
+        else
+            return String.valueOf(HttpServletResponse.SC_FORBIDDEN);
     }
 }
