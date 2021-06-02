5f97e29ec252d4b93c283ec35efd0185b70a5dc6
Luis Egui <misionpage@gmail.com>
2021-06-02 20:37:25 +0200

    * Agregado docker para los test de UserControllerIT
    Signed-off-by: Luis Egui <misionpage@gmail.com>

--- src/test/java/com/ahsoka/SALC/user_model/controller/UserControllerIT.java
+++ src/test/java/com/ahsoka/SALC/user_model/controller/UserControllerIT.java
@@ -1,18 +1,23 @@
 package com.ahsoka.SALC.user_model.controller;
 
+import com.ahsoka.SALC.user_model.UserElasticSearchContainer;
 import com.ahsoka.SALC.user_model.dtos.UserResponse;
 import com.ahsoka.SALC.user_model.filter.JwtService;
+import com.ahsoka.SALC.user_model.persistance.entity.Role;
 import com.ahsoka.SALC.user_model.persistance.entity.User;
 import com.ahsoka.SALC.user_model.persistance.repository.UserRepository;
 import com.ahsoka.SALC.user_model.service.EmailValidator;
 import com.ahsoka.SALC.user_model.service.PasswordValidatorService;
 import com.ahsoka.SALC.user_model.service.UserService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -22,21 +27,48 @@ class UserControllerIT {
     @Autowired
     private UserRepository userRepository;
 
-    private UserService userService;
-    private JwtService jwtService;
     private UserController userController;
-    private EmailValidator emailValidator;
-    private BCryptPasswordEncoder passwordEncoder;
-    private PasswordValidatorService passwordValidatorService;
+
+    @Container
+    private static ElasticsearchContainer elasticsearchContainer = new UserElasticSearchContainer();
+
+    @BeforeAll
+    static void setUp() {
+        elasticsearchContainer.start();
+    }
+
+    @AfterAll
+    static void destroy() {
+        elasticsearchContainer.stop();
+    }
+
+    private void fillUserIndex() {
+        // Admin user
+        User user = new User();
+        user.setEmail("test_admin@salc.org");
+        user.setRole(Role.ROLE_ADMIN);
+        user.setPassword("$2y$10$j.580EBjAsYNds4pJ/DUnuR1ftIoeSt0Bt5Tn87sLPXK2i1FtVSBO");
+        userRepository.save(user);
+        // Worker user
+        user = new User();
+        user.setEmail("test_worker@salc.org");
+        user.setRole(Role.ROLE_WORKER);
+        user.setPassword("$2y$10$7o0gVbIQyE8b6nrPKqFSUeMqaHfZ7UQeInxr9AjFr4CgMcAAB4KhK");
+        userRepository.save(user);
+    }
 
     @BeforeEach
     void before() {
-        jwtService = new JwtService("secret-sign", "es-salc-ucm", 36000);
-        userService = new UserService(userRepository, jwtService, emailValidator, passwordEncoder, passwordValidatorService);
+        JwtService jwtService = new JwtService("secret-sign", "es-salc-ucm", 36000);
+        EmailValidator emailValidator = new EmailValidator();
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        PasswordValidatorService passwordValidatorService = new PasswordValidatorService();
+        UserService userService = new UserService(userRepository, jwtService, emailValidator, passwordEncoder, passwordValidatorService);
         userController = new UserController(userService);
+        assertTrue(elasticsearchContainer.isRunning());
+        fillUserIndex();
     }
 
-    @Disabled
     @Test
     void testLogin_withExistingUser() {
         User usuario = new User();
@@ -46,7 +78,6 @@ class UserControllerIT {
         assertTrue(userController.login(userResponse).isPresent());
     }
 
-    @Disabled
     @Test
     void testLogin_withWrongPassword() {
         User usuario = new User();
@@ -56,7 +87,6 @@ class UserControllerIT {
         assertTrue(userController.login(userResponse).isEmpty());
     }
 
-    @Disabled
     @Test
     void testLogin_withNonExistingUser() {
         User usuario = new User();
